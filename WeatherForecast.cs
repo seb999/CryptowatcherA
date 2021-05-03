5c01dbc1cbec8882cdb7f1941e40683f88845d2d
Sebastien Dubos <sebastien.dubos@ecdc.europa.eu>
2021-05-03 15:40:14 +0200

    Upgrade to Angular11

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -11,5 +11,31 @@ namespace CryptowatcherA
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+
+
+//         ng update @angular/core@8 @angular/cli@8
+
+// ng update @angular/core@9 @angular/cli@9
+
+// npm outdated
+
+// Npm update
+
+// ng add @angular/localize
+
+// -----------------------------------
+
+// ng update --force @angular/core@10 @angular/cli@10
+
+// npm install --save @ng-select/ng-select@5.0.0
+
+// npm remove @nguniversal/module-map-ngfactory-loader
+
+// Remove codelyser
+// Remove ng-toggle-button
+// ng update @ng-bootstrap/ng-bootstrap
+// npm install ng-toggle-button
+// Npm install codelyser
+// ng update @angular/core @angular/cli
     }
 }
